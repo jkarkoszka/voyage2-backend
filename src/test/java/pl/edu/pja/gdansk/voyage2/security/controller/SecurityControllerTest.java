d5a847e390d2f0d2d37eb25540bc16bd123658ce
Jakub Karkoszka <jkarkoszka@ydp.eu>
2016-01-31 22:04:40 +0100

    Added fixed embedded redis for testing on CI environment

--- src/test/java/pl/edu/pja/gdansk/voyage2/security/controller/SecurityControllerTest.java
+++ src/test/java/pl/edu/pja/gdansk/voyage2/security/controller/SecurityControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import pl.edu.pja.gdansk.voyage2.Application;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class SecurityControllerTest extends BaseControllerTest {
 
     @Autowired
--- src/test/java/pl/edu/pja/gdansk/voyage2/user/controller/UserControllerTest.java
+++ src/test/java/pl/edu/pja/gdansk/voyage2/user/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import pl.edu.pja.gdansk.voyage2.Application;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class UserControllerTest extends BaseControllerTest {
 
     @Autowired
