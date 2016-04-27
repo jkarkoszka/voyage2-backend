898343c33ad708775a0df881cf591636b5b07f65
Jakub Karkoszka <Jakub Karkoszka>
2016-04-27 21:10:50 +0200

    deleted unnesessary field

--- src/main/java/pl/edu/pja/gdansk/voyage2/route/domain/Element.java
+++ src/main/java/pl/edu/pja/gdansk/voyage2/route/domain/Element.java
@@ -5,18 +5,9 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 abstract public class Element {
-    @DBRef
-    private Route route;
+
     private String value;
     private GeoJsonPoint point;
-
-    public Route getRoute() {
-        return route;
-    }
-
-    public void setRoute(Route route) {
-        this.route = route;
-    }
 
     public String getValue() {
         return value;
